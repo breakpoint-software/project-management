11ebbf147d4df5733f05a5356b053ff9b8c35a49
segovia <s>
2026-04-10 20:21:48 +0200

    Remove bin and obj folders and apply .gitignore

--- ProjectManagement/ProjectManagement.Tests/obj/Debug/net9.0/ProjectManagement.Tests.AssemblyInfo.cs
+++ ProjectManagement/ProjectManagement.Tests/obj/Debug/net9.0/ProjectManagement.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82af3b8070da3838c99eb82587f443bc4bf5cb85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
